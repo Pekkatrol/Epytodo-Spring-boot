2c2482f13c9bf542fcdfdefda2dd2d6a17045c7c
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-30 09:11:16 +0200

    Initial commit

---
+++ src/main/java/com/pekkatrol/epytodo/EpytodoApplication.java
@@ -0,0 +1,13 @@
+package com.pekkatrol.epytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpytodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpytodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pekkatrol/epytodo/EpytodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pekkatrol.epytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpytodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
