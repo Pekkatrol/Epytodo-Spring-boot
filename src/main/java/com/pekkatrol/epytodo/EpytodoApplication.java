e3357f8f1ae50bcaf10e37e29ea8fbe50dfd3881
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-30 09:28:46 +0200

    update: application , test, and pom file

--- src/main/java/com/pekkatrol/epytodo/EpytodoApplication.java
+++ src/main/java/com/pekkatrol/epytodo/EpytodoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EpytodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EpytodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EpytodoApplication.class, args);
+    }
 
 }
--- src/test/java/com/pekkatrol/epytodo/EpytodoApplicationTests.java
+++ src/test/java/com/pekkatrol/epytodo/EpytodoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EpytodoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
