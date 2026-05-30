e5fb31c00770cc5ba5517602ec15d4f764d61c08
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-05-30 20:35:33 +0200

    feat: add the user sql model for creation

---
+++ src/main/java/com/pekkatrol/epytodo/model/User.java
@@ -0,0 +1,40 @@
+package com.pekkatrol.epytodo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
